9d075abc1ebc522185007dbb2165f6344033df6d
failedreality <jsweber@gmail.com>
2019-05-15 08:08:56 -0500

    First commit

---
+++ first-app.js
@@ -0,0 +1,5 @@
+console.log('Hello from node.js');
+
+// const fs = require('fs');
+// fs.writeFileSync('hello.txt', 'Hello from Node.js');  //writes a file and text inside the file in the same dir
+
